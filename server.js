e5a21f371e34db465026438fd681f24644d1bba8
Christiaan Hendriksen <christiaan.hendriksen@gmail.com>
2014-12-06 16:49:59 +0000

    removed tty

--- server.js
+++ server.js
@@ -6,5 +6,4 @@ sensor = new ADC(0x48, 0x00);
 
 for (num = _i = 1; _i <= 4; num = ++_i) {
   console.log(sensor.readADCSingleEnded(0, 6144, 250));
-  console.log(sensor.readADCSingleEnded(1, 6144, 250));
 }
