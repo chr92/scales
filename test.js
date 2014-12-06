8d1d44e6407fa807f9619fd2f7c33f9a03354bf8
Christiaan Hendriksen <christiaan.hendriksen@gmail.com>
2014-12-06 16:04:47 +0000

    changed to 12 bit

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var ADC, num, sensor, _i;
 
-ADC = require("./index");
+ADC = require("./server");
 
-sensor = new ADC(0x48, 0x01);
+sensor = new ADC(0x48, 0x00);
 
 for (num = _i = 1; _i <= 4; num = ++_i) {
   console.log(sensor.readADCSingleEnded(0, 6144, 250));
